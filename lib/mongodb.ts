07e5a1d38b31b7701837b9d34e8e39f6b5e48ad9
Munna <tmunavir99@gmail.com>
2025-05-22 22:42:33 +0530

    dashboard displays atleast correct data

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,8 +1,8 @@
 // /lib/mongodb.ts
 import mongoose from "mongoose";
 
-const MONGODB_URI = "mongodb://localhost:27017/cashapp";
-// const MONGODB_URI = "mongodb+srv://vercel-admin-user:IdJXdtR0SsU9L0i9@rentalshop.sjwxy.mongodb.net/?retryWrites=true&w=majority&appName=rentalshop";
+// const MONGODB_URI = "mongodb://localhost:27017/cashapp";
+const MONGODB_URI = "mongodb+srv://vercel-admin-user:IdJXdtR0SsU9L0i9@rentalshop.sjwxy.mongodb.net/?retryWrites=true&w=majority&appName=rentalshop";
 
 // const MONGODB_URI = "mongodb+srv://vercel-admin-user:IdJXdtR0SsU9L0i9@rentalshop.sjwxy.mongodb.net/casapp?retryWrites=true&w=majority&appName=rentalshop";
 
@@ -38,3 +38,47 @@ async function dbConnect() {
 }
 
 export default dbConnect;
+
+
+// // /lib/mongodb.ts
+// import mongoose from "mongoose";
+
+// const MONGODB_URI = process.env.MONGODB_URI;
+
+// if (!MONGODB_URI) {
+//   throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
+// }
+
+// // Extend the global type to include a mongoose cache
+// interface MongooseGlobal {
+//   conn: typeof mongoose | null;
+//   promise: Promise<typeof mongoose> | null;
+// }
+
+// declare global {
+//   // Only declare if not already defined
+//   var mongoose: MongooseGlobal;
+// }
+
+// let cached = global.mongoose;
+
+// if (!cached) {
+//   cached = global.mongoose = { conn: null, promise: null };
+// }
+
+// async function dbConnect() {
+//   if (cached.conn) return cached.conn;
+
+//   if (!cached.promise) {
+//     const opts = {
+//       bufferCommands: false,
+//     };
+
+//     cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => mongoose);
+//   }
+
+//   cached.conn = await cached.promise;
+//   return cached.conn;
+// }
+
+// export default dbConnect;
